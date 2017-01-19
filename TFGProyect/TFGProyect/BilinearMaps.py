dd6f01b4f272b30ee145cf0dab763e0e3744c102
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-19 17:07:56 +0100

    Creada clase BilinearMaps.

---
+++ TFGProyect/TFGProyect/BilinearMaps.py
@@ -0,0 +1,24 @@
+class BilinearMaps(object):
+    
+    def __init__(self, p, g, gt):
+        self.p = p
+        self.g = []
+        self.gt = []
+
+    def zetaP(p):
+        z = []
+        for i in range(p):
+            z.append(i)
+        return list(z)
+
+    def e(elem1, elem2):
+        pass
+
+    def bilinear():
+        pass
+
+    def nonDegenerate():
+        pass
+
+    def computable():
+        pass
--- TFGProyect/TFGProyect/CP_ABE.py
+++ TFGProyect/TFGProyect/CP_ABE.py
@@ -1,3 +1,6 @@
+import BilinearMaps
+import _random
+
 class CP_ABE(object):
     
     def __init__(self, phi, u, s, message, structureA):
@@ -7,9 +10,28 @@ class CP_ABE(object):
         self.message = message
         self.structureA = structureA
 
-    def setup(phi,u):
-        pass
+    def setup(phi,u): #u es un número
+        pk = list()
+        h = []
+        #llamar a la función de mapa bilineal
+        while(u>0):
+            h.append(_random.Random(g)) #un g que hay de antes
+            u = u-1
+        h.sort()
+        a = _random.Random(BilinearMaps.BilinearMaps.zetaP(p))
+        alpha = _random.Random(BilinearMaps.BilinearMaps.zetaP(p))
+        
+        #actualización de valores
+        pk.append(g)
+        pk.append(gen)
+        pk.append(BilinearMaps.BilinearMaps.e(gen,gen)**alpha)
+        pk.append(gen**a)
+        pk.append(h)
+        msk = gen**alpha
+
         #devuelve pk y msk
+        return pk, msk
+        
 
     def keyGen(msk,s):
         pass
